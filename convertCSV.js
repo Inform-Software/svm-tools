dc18bfc782a300c6e399b9f26ca386387a88090b
Jonathan Diehl <jonathan.diehl@inform-software.com>
2015-05-19 17:49:07 +0200

    added csv-to-json conversion script

---
+++ convertCSV.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+var q = require('q');
+var minimist = require('minimist');
+var util = require('./svm-tools/util');
+
+var args = minimist(process.argv.splice(2));
+if (args._.length < 2) {
+  console.log('Usage: convertCSV [test.csv] [test.json]');
+  process.exit(-1);
+}
+
+var testdata = args._[0];
+var outFile = args._[1];
+
+util.readCSV(testdata)
+  .then(function (data) { return q.nfcall(fs.writeFile, outFile, JSON.stringify(data)); })
+  .done();
